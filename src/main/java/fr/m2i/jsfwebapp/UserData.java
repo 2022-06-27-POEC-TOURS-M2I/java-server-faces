f8d300b1d12d779eefd87b2b4f6a421eafcde9d2
Tiity <cadet_a@etna-alternance.net>
2022-08-16 10:43:04 +0200

    Formulaire ajout employe

--- src/main/java/fr/m2i/jsfwebapp/UserData.java
+++ src/main/java/fr/m2i/jsfwebapp/UserData.java
@@ -7,7 +7,7 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean(name = "userData", eager = true)
 public class UserData {
 
-    private final List<Employee> employees = new ArrayList() {
+    private static final List<Employee> employees = new ArrayList() {
         {
             add(new Employee("Anthony", "IT", 28, 3000.00));
             add(new Employee("John", "Marketing", 42, 3700.00));
@@ -15,7 +15,60 @@ public class UserData {
         }
     };
 
+    private String name;
+    private String department;
+    private int age;
+    private double salary;
+
+    public String addEmployee() {
+
+        // Ajout d'un employé
+        Employee employee = new Employee(name, department, age, salary);
+        employees.add(employee);
+
+        // Reset les données du formulaire
+        name = null;
+        department = null;
+        age = 0;
+        salary = 0d;
+
+        // On ré affiche la page data
+        return null;
+    }
+
     public List<Employee> getEmployees() {
         return employees;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
     }
 }
